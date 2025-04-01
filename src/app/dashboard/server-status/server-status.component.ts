ce67b431f3008fcc04c7a69d56d3dac47b8d1f27
shikhor <shikhor.roy@cefalo.com>
2025-04-01 10:24:21 +0600

    decorate server status component

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,6 +7,27 @@ import {Component} from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit, OnDestroy {
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  private timeOutId?: ReturnType<typeof setTimeout>;
+
+  ngOnInit(): void {
+    const TIMEOUT = 3000;
+    this.timeOutId = setTimeout(() => {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        this.currentStatus = 'online';
+      } else if (randomNumber < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, TIMEOUT);
+  }
+
+  ngOnDestroy(): void {
+    if (this.timeOutId) {
+      clearTimeout(this.timeOutId);
+    }
+  }
 }
