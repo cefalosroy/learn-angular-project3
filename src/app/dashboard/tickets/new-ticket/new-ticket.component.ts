d77d0b1ff6b4d9e7eca3f8bcd8522575a525c259
shikhor <shikhor.roy@cefalo.com>
2025-04-01 10:31:31 +0600

    form submit

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {ButtonComponent} from '../../../shared/button/button.component';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
   imports: [
-    ButtonComponent
+    ButtonComponent,
+    FormsModule
   ],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  ticket = {
+    title: '',
+    description: ''
+  };
 
+  onSubmit() {
+    console.log(this.ticket);
+    this.form?.nativeElement.reset();
+  }
 }
